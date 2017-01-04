59f92b015d14b1a371dd10798f8967560572f624
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-01-04 22:11:05 +0900

    update structure a bit

--- lib/github_hook_bot/github/hook.ex
+++ lib/github_hook_bot/github/hook.ex
@@ -3,7 +3,11 @@ defmodule GithubHookBot.Github.Hook do
   Storeing hooked data.
   """
 
+  alias Plug.Conn
   alias GithubHookBot.Github.Repository
+
+  @type hook_struct :: %__MODULE__{}
+  @type conn :: Conn.__struct__
 
   @doc """
   `:event` is event in X-GitHub-Event.
@@ -17,4 +21,23 @@ defmodule GithubHookBot.Github.Hook do
     :action,
     repository: %Repository{},
   ]
+
+  # TODO: ここにWebhookを受け取った時の構造体への保存処理を入れていく
+  @spec received_hook(conn) :: {:ok, hook_struct} | {:error, any}
+  def received_hook(conn) do
+      case Poison.decode(conn.resp_body) do
+        {:ok, json_body} ->
+          %__MODULE__{}
+          |> Map.put(:event, conn.resp_headers) # for example
+          |> Map.put(:payload, json_body)
+          |> Map.put(:action, conn.neko)
+          |> put_in([Access.key(:repository), Access.key(:full_name)], json_body.repo.full_name)
+        {:error, message} ->
+          %__MODULE__{}
+          |> Map.put(:event, conn.resp_headers)
+          |> Map.put(:payload, message <> ": " <> conn.resp_body)
+          |> Map.put(:action, conn.neko)
+      end
+    end
+  end
 end
